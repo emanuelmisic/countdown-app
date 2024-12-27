7413b36eb1441f6b3b57f482ca055a389e31e3df
emanuelmisic <emanuelmisic64@gmail.com>
2024-12-27 21:38:35 +0100

    deployment changes

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  base: "/countdown-app/",
+  publicPath: "/countdown-app/",
   transpileDependencies: true,
 });
